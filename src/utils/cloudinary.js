3223363bb87ca3d337fd44a6a404360f8af8eab0
Tanvi Singh <11j38tanvisingh@gmail.com>
2025-09-07 13:52:37 +0530

    Add cloudinary.js

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,41 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+// Configure cloudinary with your credentials
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded on cloudinary. File src: " + response.url);
+    // Once the file is uploaded, we would like to delete it from our server
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    console.log("Error on Cloudinary", error);
+    if (localFilePath && fs.existsSync(localFilePath)) {
+      fs.unlinkSync(localFilePath);
+    }
+    throw error;
+  }
+};
+
+const deleteFromCloudinary = async (publicId) => {
+  try {
+    const result = await cloudinary.uploader.destroy(publicId);
+    console.log("Deleted from Cloudinary. Public id: ", publicId);
+  } catch (error) {
+    console.log("Error deleting from Cloudinary", error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
